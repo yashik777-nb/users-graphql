1e18d53887b0a10baf0a6c5f2a4e4385aee8f736
Yash <yaswanth.iddibani@gmail.com>
2022-04-16 17:22:15 +0100

    RootSchema-Implementation

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: "23", firstName: "Bill", age: 20 },
+  { id: "32", firstName: "Sam", age: 48 },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,4 +15,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
 
+const schema = require("./schema/schema");
+
 const app = express();
 
 app.use(
   "/graphql",
   expressGraphQL({
     graphiql: true,
+    schema,
   })
 );
 
