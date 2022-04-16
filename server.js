3263603bb0db65424c54ed13e46bec9585e21b12
Yash <yaswanth.iddibani@gmail.com>
2022-04-16 13:24:28 +0100

    Added USer Schema

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => console.log("Listening on 4000"));
